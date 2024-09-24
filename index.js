ce0f27605da222fc3f799e1f14aba06cf13651bb
Ariana <152944814+aam214@users.noreply.github.com>
2024-09-24 13:47:30 -0500

    Edited setTimeout to have timer variable. ClearTimeout(timer) to nextButton and backButton.

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const nextButton = document.getElementById("next-button");
 const backButton = document.getElementById("back-button");
 //for transform
 let n = 0;
+let timer;
 
 
 backButton.addEventListener("click", () => {
 //45 degrees
   n = n + 45;
+  clearTimeout(timer);
   updateImageGallery();
 });
 
 nextButton.addEventListener("click", () => {
   n = n - 45; 
+  clearTimeout(timer);
   updateImageGallery();
 })
 
@@ -20,7 +23,7 @@ let updateImageGallery = () => {
   photoContainer.style.transform =
 `perspective(1000px) rotateY(${n}deg)`;
 
-setTimeout(() => {
+timer = setTimeout(() => {
   n = n - 45;
   updateImageGallery();
 }, 3500);
