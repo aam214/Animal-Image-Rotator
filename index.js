6bf6695fbecf7361bd1300b1375a986f5d2a651b
Ariana <152944814+aam214@users.noreply.github.com>
2024-09-24 13:42:43 -0500

    Changed the x variable to n. Added updateImageGallery function with  style.transform and a setTImeout function. Added transition to the transform in the .container in CSS.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const photoContainer = document.querySelector(".container");
 const nextButton = document.getElementById("next-button");
 const backButton = document.getElementById("back-button");
-let x = 0;
+//for transform
+let n = 0;
 
 
 backButton.addEventListener("click", () => {
-
-})
+//45 degrees
+  n = n + 45;
+  updateImageGallery();
+});
 
 nextButton.addEventListener("click", () => {
-
+  n = n - 45; 
+  updateImageGallery();
 })
+
+let updateImageGallery = () => {
+  photoContainer.style.transform =
+`perspective(1000px) rotateY(${n}deg)`;
+
+setTimeout(() => {
+  n = n - 45;
+  updateImageGallery();
+}, 3500);
+}
+updateImageGallery();
