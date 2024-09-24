a2df1a0c8093ce44391b2a82fcf13a517c367bd9
Ariana <152944814+aam214@users.noreply.github.com>
2024-09-24 13:08:38 -0500

    Query Selector and getElementID for container and buttons. Added click event listeners for buttons. Google Fonts placed in the head. Changed font of h1.  Added transform to img element in CSS. Edited button transition, hover, padding, and margin.

---
+++ index.js
@@ -0,0 +1,13 @@
+const photoContainer = document.querySelector(".container");
+const nextButton = document.getElementById("next-button");
+const backButton = document.getElementById("back-button");
+let x = 0;
+
+
+backButton.addEventListener("click", () => {
+
+})
+
+nextButton.addEventListener("click", () => {
+
+})
